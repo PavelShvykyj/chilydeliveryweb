d2cb28f40b73d9c44478c398a15dd31cfa82fcde
PavelShvykyj <myserver1978@gmail.com>
2022-04-03 22:42:38 +0300

    next row edit logil in cruely table

--- src/app/order/order-goods-list/order-goods-list.component.ts
+++ src/app/order/order-goods-list/order-goods-list.component.ts
@@ -17,81 +17,81 @@ import { DialogstringinputComponent } from 'src/app/baseelements/dialogstringinp
   templateUrl: './order-goods-list.component.html',
   styleUrls: ['./order-goods-list.component.scss']
 })
-export class OrderGoodsListComponent implements OnInit  {
+export class OrderGoodsListComponent implements OnInit {
 
-  displayedColumns : string[] = ['good' ,'quantity','price' ,'comment','buttonsgroup'];
-  displayedCutleryColumns : string[] = ['name' ,'quantity'];
+  displayedColumns: string[] = ['good', 'quantity', 'price', 'comment', 'buttonsgroup'];
+  displayedCutleryColumns: string[] = ['name', 'quantity'];
 
-  EditCellsChain : IDictionary<string> = {
-    'quantity':'comment',
-    'comment':''
+  EditCellsChain: IDictionary<string> = {
+    'quantity': 'comment',
+    'comment': ''
   };
 
-  EditCutleryCellsChain : IDictionary<string> = {
-    'quantity':''
+  EditCutleryCellsChain: IDictionary<string> = {
+    'quantity': ''
   };
 
 
-  dataSource : MatTableDataSource<IOrderGoodsWievRecordWithEntity>  = new MatTableDataSource([]);
-  cutleryDataSourse : MatTableDataSource<IOrderWievCutlery>  = new MatTableDataSource([]);
-  ordersusbs:Subscription;
-  filial : string = '';
-  paytype : string = '';
-  filialsubs : Subscription;
-  cutlerysubs : Subscription;
-  total:number = 0;
+  dataSource: MatTableDataSource<IOrderGoodsWievRecordWithEntity> = new MatTableDataSource([]);
+  cutleryDataSourse: MatTableDataSource<IOrderWievCutlery> = new MatTableDataSource([]);
+  ordersusbs: Subscription;
+  filial: string = '';
+  paytype: string = '';
+  filialsubs: Subscription;
+  cutlerysubs: Subscription;
+  total: number = 0;
 
 
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  @ViewChild("GoodsTab", {static: false}) GoodsTab: MatTab;
+  @ViewChild("GoodsTab", { static: false }) GoodsTab: MatTab;
 
-  @ViewChild("inputCell",{static:false}) inputCell: TemplateRef<any>;
-  @ViewChild('outputCell',{static:false}) outputCell: TemplateRef<any>;
+  @ViewChild("inputCell", { static: false }) inputCell: TemplateRef<any>;
+  @ViewChild('outputCell', { static: false }) outputCell: TemplateRef<any>;
 
-  @ViewChild('editmatinput',{static:false}) editmatinput: MatInput;
+  @ViewChild('editmatinput', { static: false }) editmatinput: MatInput;
 
   constructor(private store: Store<AppState>,
-              public dialog: MatDialog ) { }
+    public dialog: MatDialog) { }
 
   ngOnInit() {
     this.ordersusbs = this.store.pipe(select(selectAllOrderGoodsWithEntity),
-    map(orderrecords=> {
-      let wievorderrecords : Array<IOrderGoodsWievRecordWithEntity> = orderrecords.map(el => {
-        return {...el, EditCellName : "", NextCellEdit: ""}
+      map(orderrecords => {
+        let wievorderrecords: Array<IOrderGoodsWievRecordWithEntity> = orderrecords.map(el => {
+          return { ...el, EditCellName: "", NextCellEdit: "" }
+        })
+        return wievorderrecords;
       })
-      return wievorderrecords;
-    })
     )
-    .subscribe(orderrecords=>{
+      .subscribe(orderrecords => {
 
-      this.dataSource.data=orderrecords;
-      this.total = this.GetOrderTotal();
-    });
-
-    this.filialsubs = this.store.pipe(select(selectOrderFilial))
-    .subscribe(data=>{
-      this.filial = data.filial;
-      this.paytype = data.paytype;
-    });
-
-    this.cutlerysubs = this.store.pipe(select(selectOrderCutlery))
-    .subscribe(data=>{
-      let OrderCutlery : IOrderCutlery[] = JSON.parse(data);
-      let OrderWievCutlery : IOrderWievCutlery[] = [];
-      OrderCutlery.forEach(element => {
-        OrderWievCutlery.push({...element, EditCellName : "", NextCellEdit: ""})
+        this.dataSource.data = orderrecords;
+        this.total = this.GetOrderTotal();
       });
 
-      this.cutleryDataSourse.data = OrderWievCutlery;
-    });
+    this.filialsubs = this.store.pipe(select(selectOrderFilial))
+      .subscribe(data => {
+        this.filial = data.filial;
+        this.paytype = data.paytype;
+      });
+
+    this.cutlerysubs = this.store.pipe(select(selectOrderCutlery))
+      .subscribe(data => {
+        let OrderCutlery: IOrderCutlery[] = JSON.parse(data);
+        let OrderWievCutlery: IOrderWievCutlery[] = [];
+        OrderCutlery.forEach(element => {
+          OrderWievCutlery.push({ ...element, EditCellName: "", NextCellEdit: "" })
+        });
+
+        this.cutleryDataSourse.data = OrderWievCutlery;
+      });
 
   }
 
-  GetOrderTotal():number {
+  GetOrderTotal(): number {
     let total = 0;
-    this.dataSource.data.forEach(record => total=total+record.quantity*record.good.price);
+    this.dataSource.data.forEach(record => total = total + record.quantity * record.good.price);
     return total;
   }
 
@@ -108,56 +108,56 @@ export class OrderGoodsListComponent implements OnInit  {
     this.ordersusbs.unsubscribe();
   }
 
-  IncrQuantity(record:IOrderGoodsRecord) {
+  IncrQuantity(record: IOrderGoodsRecord) {
     record.quantity = 1;
-    this.store.dispatch(UpsertOrderRecord({record}));
+    this.store.dispatch(UpsertOrderRecord({ record }));
   }
 
   DecrQuantity(record) {
     record.quantity = -1;
-    this.store.dispatch(UpsertOrderRecord({record}));
+    this.store.dispatch(UpsertOrderRecord({ record }));
 
   }
 
-  OnRecordClick(record ) {
+  OnRecordClick(record) {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
-    dialogConfig.autoFocus=true;
-    dialogConfig.minHeight="25wh"
-    dialogConfig.minWidth="25wv"
+    dialogConfig.autoFocus = true;
+    dialogConfig.minHeight = "25wh"
+    dialogConfig.minWidth = "25wv"
 
-    dialogConfig.data = {title: `Комментарий для : ${record.good.name}` , answer:record.comment}
+    dialogConfig.data = { title: `Комментарий для : ${record.good.name}`, answer: record.comment }
 
-    const DialogRef : MatDialogRef<DialogstringinputComponent>  = this.dialog.open(
+    const DialogRef: MatDialogRef<DialogstringinputComponent> = this.dialog.open(
       DialogstringinputComponent,
       dialogConfig);
-      DialogRef.afterClosed().pipe(first()).subscribe(res =>{
+    DialogRef.afterClosed().pipe(first()).subscribe(res => {
       record.comment = res.answer;
       record.quantity = 0;
-      this.store.dispatch(UpsertOrderRecord({record}));
+      this.store.dispatch(UpsertOrderRecord({ record }));
     });
 
 
   }
 
   OnFilialChange() {
-    this.store.dispatch(UpdateOrderfilial({filial:this.filial}));
+    this.store.dispatch(UpdateOrderfilial({ filial: this.filial }));
   }
 
   OnPaytypeChange() {
-    this.store.dispatch(UpdateOrderpaytype({paytype:this.paytype}));
+    this.store.dispatch(UpdateOrderpaytype({ paytype: this.paytype }));
     this.OnFilialChange();
   }
 
   Del(record) {
-    this.store.dispatch(DeleteOrderRecord({recordid:record.id}));
+    this.store.dispatch(DeleteOrderRecord({ recordid: record.id }));
   }
 
 
-  OnCellClick(record:IOrderGoodsWievRecordWithEntity,cellName:string) {
+  OnCellClick(record: IOrderGoodsWievRecordWithEntity, cellName: string) {
     record.EditCellName = cellName;
     record.NextCellEdit = cellName === "" ? "" : this.EditCellsChain[cellName];
-    setTimeout(()=>{
+    setTimeout(() => {
       if (this.editmatinput != undefined) {
         if (!this.editmatinput.focused) {
 
@@ -169,29 +169,59 @@ export class OrderGoodsListComponent implements OnInit  {
 
   }
 
-  OnCutleryCellClick(record,cellName:string) {
-    record.EditCellName = cellName;
-    record.NextCellEdit = cellName === "" ? "" : this.EditCutleryCellsChain[cellName];
-    let lastID = this.cutleryDataSourse.data[this.cutleryDataSourse.data.length-1].id
-    console.log('lastID',lastID);
-    if  (record.id != lastID) {
-      let nextId = (parseInt(record.id)+1);
-      console.log('nextId',nextId);
-      if (this.cutleryDataSourse.data.length >= nextId) {
-        this.cutleryDataSourse.data = this.cutleryDataSourse.data.map(element => {
-          console.log(element.id);
-          if (element.id == nextId.toString()) {
-            console.log(element.id,element.EditCellName);
-            element.EditCellName = 'quantity';
+  OnCutleryCellClick(record, cellName: string, goNext = true,nextRow=false) {
+    // goNext - только на клавишу  ентер и первый клик переход по ячейкам и строкам
+    // nextRow=false - на первый клик переход на следующую строку не выполняем
+    
+    // not enter 
+    if (!goNext) {
+      //  ушли из редактирования обнудяем редактируемость и сохраняем таблицу
+      this.cutleryDataSourse.data = this.cutleryDataSourse.data.map(element => {
+        element.EditCellName = "";
+        element.NextCellEdit = "";
 
-          }
-          return element;
-        });
-      }
-      console.log('data',this.cutleryDataSourse.data);
+        return element;
+      });
+      
+      //  вызов сохранения в стейт
+      
+      return;
     }
 
-    setTimeout(()=>{
+    // enter
+    
+    let lastID = this.cutleryDataSourse.data[this.cutleryDataSourse.data.length - 1].id
+    let nextId = (parseInt(record.id) + 1);
+    let nextIdStr = nextId.toString();
+    let dataLenth = this.cutleryDataSourse.data.length;
+    let NextCellEdit = cellName === "" ? "" : this.EditCutleryCellsChain[cellName];
+    // переход на след строку если не указано иного , ячеек в текущейстроке на редактируем и след строка существует
+    let GoNextRow = (nextRow && NextCellEdit == "" && cellName == "" && record.id != lastID && dataLenth >= nextId);
+
+    this.cutleryDataSourse.data = this.cutleryDataSourse.data.map(element => {
+      if (element.id == record.id) {
+        element.EditCellName = cellName;
+        element.NextCellEdit = NextCellEdit;
+      }
+      else {
+        //  last cell edit may go net row
+        if (GoNextRow && element.id == nextIdStr) {
+          element.EditCellName = 'quantity';
+        }
+        else {
+          element.EditCellName = "";
+        }
+        element.NextCellEdit = "";
+      }
+
+      return element;
+    });
+
+    if (!GoNextRow && NextCellEdit == "") {
+      //  вызов сохранения в стейт
+    }
+
+    setTimeout(() => {
       if (this.editmatinput != undefined) {
         if (!this.editmatinput.focused) {
 
@@ -202,23 +232,25 @@ export class OrderGoodsListComponent implements OnInit  {
   }
 
   OnEditFocus(event) {
-    console.log("event",event);
-    setTimeout(()=>event.target.select(),20);
+
+    setTimeout(() => event.target.select(), 20);
   }
 
-  OnEditLeave(record) {
-    console.log('GoodsTab.isActive',this.GoodsTab.isActive);
-    if(this.GoodsTab.isActive ) {
-      this.OnCellClick(record,record.NextCellEdit);
+  OnEditLeave(record, from: string) {
+
+
+    if (this.GoodsTab.isActive) {
+      this.OnCellClick(record, record.NextCellEdit);
     } else {
-      this.OnCutleryCellClick(record,record.NextCellEdit)
+
+      this.OnCutleryCellClick(record, record.NextCellEdit, from != 'blur',true);
     }
   }
 
 
 
 
-  }
+}
 
 
 
